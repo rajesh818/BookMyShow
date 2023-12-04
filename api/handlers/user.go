961649a802855ac9af0d03fab6625857d6c91acf
rajesh818 <ambatigururajesh818@gmail.com>
2023-12-04 13:59:16 +0530

    Complete user routes

--- api/handlers/user.go
+++ api/handlers/user.go
@@ -2,10 +2,12 @@ package handlers
 
 import (
 	"encoding/json"
-	"github.com/gorilla/mux"
-	"github.com/rajesh818/BookMyShow/api/models"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rajesh818/BookMyShow/api/models"
+	"gorm.io/gorm"
 )
 
 type UserDetails struct {
@@ -24,6 +26,56 @@ func GetUserById(w http.ResponseWriter, r *http.Request) {
 	if result.Error != nil {
 		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
 		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(user); err != nil {
+		http.Error(w, "Error encoding JSON response", http.StatusInternalServerError)
+		return
+	}
+}
+
+func GetUserByEmail(w http.ResponseWriter, r *http.Request) {
+	log.Println("Get user by email started")
+	vars := mux.Vars(r)
+	email := vars["email"]
+	var user models.User
+	result := DB.Where("email = ?", email).First(&user)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(user); err != nil {
+		http.Error(w, "Error encoding JSON response", http.StatusInternalServerError)
+		return
+	}
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	queryParams := r.URL.Query()
+
+	email := queryParams.Get("email")
+	id := queryParams.Get("id")
+	var result *gorm.DB
+	var user models.User
+	if id == "" && email == "" {
+		http.Error(w, "No email or Id available to fetch data", http.StatusBadRequest)
+		return
+	}
+	if id == "" {
+		result = DB.Where("email = ?", email).First(&user)
+		if result.Error != nil {
+			http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else {
+		result = DB.Where("id = ?", id).First(&user)
+		if result.Error != nil {
+			http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
@@ -59,4 +111,23 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error encoding JSON response", http.StatusInternalServerError)
 		return
 	}
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request){
+	log.Println("GetUserById Function reached")
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var user models.User
+	result := DB.Where("id = ?", id).Delete(&user);
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(user); err != nil {
+		http.Error(w, "Error encoding JSON response", http.StatusInternalServerError)
+		return
+	}
+
 }
--- api/routes/user.go
+++ api/routes/user.go
@@ -10,6 +10,20 @@ import (
 
 func UserRoutes(r *mux.Router) {
 	log.Println("UserRoutes")
-	r.HandleFunc("/get/{id}", handlers.GetUserById).Methods(http.MethodGet)
 	r.HandleFunc("/create", handlers.CreateUser).Methods(http.MethodPost)
+
+	// delete a user from db
+	r.HandleFunc("/delete/{id}", handlers.DeleteUser).Methods(http.MethodDelete)
+
+	// get user by email
+	r.HandleFunc("/getByEmail/{email}", handlers.GetUserByEmail).Methods(http.MethodGet)
+
+	// get user by id
+	r.HandleFunc("/getById/{id}", handlers.GetUserById).Methods(http.MethodGet)
+
+	// Get user by email/id
+	r.HandleFunc("/get", handlers.GetUser).Methods(http.MethodGet)
+
+
+
 }
